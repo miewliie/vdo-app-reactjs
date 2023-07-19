5901a99c8d180d26a512ab87bf50ebd19738780c
miewliie <miewliie@gmail.com>
2023-07-18 21:40:05 -0700

    [e2e] Add more tests and refactored it.

--- cypress/e2e/video.cy.js
+++ cypress/e2e/video.cy.js
@@ -4,61 +4,73 @@ describe('video scenarios', () => {
         cy.visit('http://localhost:3000/');
       })
 
-    context.only('feed: ', () => {
+    context('feed: ', () => {
+      
+      it('click first video to correct video player', () => {
         let videoTitle;
 
-        it('click first video to video player', () => {
-    
-          // click first video
-          cy.getBySel("videoTitle")
-            .first()
-            .invoke('text')
-            .then((text) =>{
-              videoTitle = text;
-            })
-          cy.getBySel("videoUrl").first().click();
-    
-          // on video player detail
-          cy.getBySel("videoPlayer").should('be.visible');
-          cy.intercept({
-            method: 'GET',
-            url: /https:\/\/googleads\.g\.doubleclick\.net\/.*/,
-          }, (req) => {
-            req.reply({}) // Empty response to ignore the request
-          })
-          cy.intercept('GET', 'https://www.youtube.com/*').as('youtubeRequests')
-          cy.wait('@youtubeRequests') 
-    
-          // // verify video title match
-          cy.getBySel("videoPlayerTitle")
-            .invoke('text')
-            .then((text) =>{
-              const videoPlayerTitle = text;
-              expect(videoTitle).to.equal(videoPlayerTitle)
-            })
-    
+        cy.getFirstVideoTitleFromVideoList("videoTitle", (returnvalue) => {
+          videoTitle = returnvalue;
         })
+        cy.getBySel("videoUrl").first().click();
 
-        it('click first video of 3rd category to video player', () => {
-          let thirdCat;
-
-          cy.getBySel("categories")  
-          .first()
-          .next()
-          .next()
-          .should(($element) => {
-            thirdCat = $element.text();
-            $element.click();
-          })
-          cy.wait(5000)
-          cy.getBySel("videoUrl").first().click()
-
-          cy.getVideoPlayerTitle((returnvalue) => {
-              expect(returnvalue).to.include(thirdCat);
-          })
-        });
+        cy.getVideoPlayerTitle((returnvalue) => {
+          expect(returnvalue).to.include(videoTitle);
+        })   
       })
 
+      it('click first video of 3rd category to correct video player', () => {
+        let thirdCat;
+
+        cy.getBySel("categories")  
+        .first()
+        .next()
+        .next()
+        .should(($element) => {
+          thirdCat = $element.text();
+          $element.click();
+        })
+        cy.wait(10000)
+        cy.getBySel("videoUrl").first().click()
+
+        cy.getVideoPlayerTitle((returnvalue) => {
+            expect(returnvalue).to.include(thirdCat);
+        })
+      });
+      })
+    
+    context('channel detail: ', () => {
+        it('click first video to correct video player', () => {
+          let videoTitle;
+
+          cy.visit('/channel/UC3IZKseVpdzPSBaWxBxundA')
+          cy.getFirstVideoTitleFromVideoList("videoTitle", (returnvalue) => {
+            videoTitle = returnvalue;
+          })
+          cy.getBySel("videoUrl").first().click();
+
+          cy.getVideoPlayerTitle((returnvalue) => {
+            expect(returnvalue).to.include(videoTitle);
+          })  
+        });
+
+    })
+
+    context('search results: ', () => {
+        it('click first video to correct video player', () => {
+          let videoTitle;
+
+          cy.visit('/search/cooking')
+          cy.getFirstVideoTitleFromVideoList("videoTitle", (returnvalue) => {
+            videoTitle = returnvalue;
+          })
+          cy.getBySel("videoUrl").first().click();
+
+          cy.getVideoPlayerTitle((returnvalue) => {
+            expect(returnvalue).to.include(videoTitle);
+          })  
+        });
+    })
 
 
 })
