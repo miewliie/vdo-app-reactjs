c20bd4337c1f95caa531c1bb34484c439c3a21c6
miewliie <miewliie@gmail.com>
2023-07-18 20:39:34 -0700

    [e2e] add getVideoPlayerTitle custom command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,23 @@
 Cypress.Commands.add("getBySel", (selector, ...args) => {
     return cy.get(`[data-test-id=${selector}]`, ...args);
   });
+
+Cypress.Commands.add('getVideoPlayerTitle',  (callback) => {
+    // on video player detail
+    cy.getBySel("videoPlayer").should('be.visible');
+    cy.intercept({
+      method: 'GET',
+      url: /https:\/\/googleads\.g\.doubleclick\.net\/.*/,
+    }, (req) => {
+      req.reply({}) // Empty response to ignore the request
+    })
+    cy.intercept('GET', 'https://www.youtube.com/*').as('youtubeRequests')
+    cy.wait('@youtubeRequests') 
+
+    // return title of that video from video player
+    cy.getBySel("videoPlayerTitle")
+      .invoke('text')
+      .then((text) =>{
+        callback(text);
+      })
+});
